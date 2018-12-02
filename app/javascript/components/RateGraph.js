aad3920fb0143c6c4324c92175d2a02e1a3e41b4
Edward Minnett <ed@methodic.io>
2018-12-02 18:16:58 +0000

    feat: Improve client-side results readability

--- app/javascript/components/RateGraph.js
+++ app/javascript/components/RateGraph.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from 'react-redux';
+import { formatDate } from '../utils/exchange_rates_api';
 import { Line as LineChart} from 'react-chartjs-2';
 import Chart from 'chart.js';
 
@@ -19,10 +20,25 @@ function mapStateToProps (state) {
     .filter(c => c.code === state.counterCurrency)[0].symbol;
   let data = [];
   let labels = [];
+  let pointRadii = [];
+  let pointColours = [];
+  let pointBorderWidths = [];
   Object.values(state.graphData).map((datum) => {
     data.push((datum.value * state.amount).toFixed(2));
     labels.push(datum.date);
+    
+    // Populate point styling arrays so the selected date is highlighted.
+    if (datum.date === formatDate(state.selectedDate)) {
+      pointRadii.push(5);
+      pointColours.push('rgba(255,255,255, 0.8)');
+      pointBorderWidths.push(2);
+    } else {
+      pointRadii.push(2);
+      pointColours.push('rgba(21,106,11, 0.6)');
+      pointBorderWidths.push(1);
+    }
   })
+
   const min = Math.min(Object.values(data)) * 0.9;
   const max = Math.max(Object.values(data)) * 1.1;
 
@@ -38,7 +54,16 @@ function mapStateToProps (state) {
   return {
     selectedDate: state.selectedDate,
     data: {
-      datasets: [{label, data, borderColor: '#156A0B', backgroundColor: 'rgba(21,106,11, 0.3)'}],
+      datasets: [{
+        label,
+        data,
+        borderColor: '#156A0B',
+        backgroundColor: 'rgba(21,106,11, 0.3)',
+        lineTension: 0,
+        pointRadius: pointRadii,
+        pointBackgroundColor: pointColours,
+        pointBorderWidth: pointBorderWidths
+      }],
       labels
     },
     options: {
--- app/javascript/components/Results.js
+++ app/javascript/components/Results.js
@@ -2,17 +2,54 @@ import React from "react";
 import { connect } from 'react-redux';
 
 class Results extends React.Component {
+
+  readableDate (date) {
+    const d = typeof date === 'string' ? new Date(date) : date;
+    const dayLabels = ['Sunday', 'Monday', 'Tuesday', 'Wednesday',
+      'Thursday', 'Friday', 'Saturday']
+    const monthLabels = ['Januray', 'February', 'March', 'April', 'May', 'June',
+      'July', 'August', 'September', 'October', 'November', 'December']
+    const weekDay = dayLabels[d.getDay()];
+    const day = this.ordinalNumber(d.getDate());
+    const month = monthLabels[d.getMonth()];
+    const year = d.getFullYear();
+
+    return `${weekDay} the ${day} of ${month}, ${year}`
+  }
+
+  ordinalNumber (x) {
+    var j = x % 10,
+        k = x % 100;
+    if (j == 1 && k != 11) {
+      return x + "st";
+    }
+    if (j == 2 && k != 12) {
+      return x + "nd";
+    }
+    if (j == 3 && k != 13) {
+      return x + "rd";
+    }
+    return x + "th";
+  }
+
+  delimitedNumber (x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
   render () {
-    const baseMoney = `${this.props.baseCurrency.symbol}${this.props.amount} ` +
+    const baseAmount = this.delimitedNumber(this.props.amount);
+    const exchangedAmount = this.delimitedNumber(
+      (this.props.amount * this.props.exchangeRate).toFixed(2)
+    );
+    const baseMoney = `${this.props.baseCurrency.symbol}${baseAmount}\xa0` +
       `(${this.props.baseCurrency.code})`;
     const exchangedMoney = `${this.props.counterCurrency.symbol}` +
-      `${(this.props.amount * this.props.exchangeRate).toFixed(2)} ` +
-      `(${this.props.counterCurrency.code})`;
+      `${exchangedAmount}\xa0(${this.props.counterCurrency.code})`;
 
     return (
       <div className='results'>
-        {`On ${this.props.date},`}
-        <br />
+        {`On ${this.readableDate(this.props.date)},`}
+        <span className='conditional-line-break'></span>
         {`${baseMoney} was worth ${exchangedMoney}.`}
       </div>
     );
