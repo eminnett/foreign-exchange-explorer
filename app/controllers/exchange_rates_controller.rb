c306edce48543ae91fcf090445c897338c108c56
Edward Minnett <ed@methodic.io>
2018-12-02 15:08:45 +0000

    refactor: Tidy up ExchangeRatesController

--- app/controllers/exchange_rates_controller.rb
+++ app/controllers/exchange_rates_controller.rb
@@ -1,47 +1,55 @@
 # frozen_string_literal: true
 
 class ExchangeRatesController < ApplicationController
-  def show
-    base_currency = exchange_rate_params[:base_currency]
-    counter_currency = exchange_rate_params[:counter_currency]
-    on = exchange_rate_params[:on]
-    # Default to the range between today and 90 days ago.
-    from = exchange_rate_params[:from] || Time.zone.today - 90.days
-    to = exchange_rate_params[:to] || Time.zone.today
+  CURRENCY_PARAMS = %i[base_currency counter_currency].freeze
+  DATE_PARAMS = %i[on from to].freeze
 
-    # Since there are defaults for 'from' and 'to',
-    # providing 'on' trumps these defaults.
-    if on
-      begin
-        response = ExchangeRate.find_rate(on, base_currency, counter_currency)
-      rescue ExchangeRate::NotFound => e
-        response = {error: e.message}
-      end
-    elsif from && to
-      response = begin
-        if from > to
-          {error: "The 'from' date must be older than the 'to' date."}
-        else
-          ExchangeRate.rates_between(from, to, base_currency, counter_currency)
-        end
-      end
-    end
-    render json: response
+  attr_accessor(*(CURRENCY_PARAMS + DATE_PARAMS))
+
+  def show
+    assign_params parse_params
+    assign_defaults
+    render json: build_response
   end
 
   private
 
-  def exchange_rate_params
-    currencies = %i[base_currency counter_currency]
-    dates = %i[on from to]
-    permitted_params = params.permit(*(currencies + dates))
-    dates.each do |param|
-      permitted_params[param] = (
-        permitted_params[param].present? ?
-          Date.parse(permitted_params[param]) :
-          nil
-      )
+  def parse_params
+    permitted_params = params.permit(*(CURRENCY_PARAMS + DATE_PARAMS))
+    DATE_PARAMS.each do |date_param|
+      date = permitted_params[date_param]
+      permitted_params[date_param] = Date.parse(date) if date.present?
     end
     permitted_params
+  end
+
+  def assign_params(permitted_params)
+    (CURRENCY_PARAMS + DATE_PARAMS).each do |p|
+      instance_variable_set("@#{p}", permitted_params[p])
+    end
+  end
+
+  def assign_defaults
+    @from = from || Time.zone.today - 90.days
+    @to = to || Time.zone.today
+  end
+
+  def build_response
+    # Since there are defaults for 'from' and 'to', providing 'on' trumps these defaults.
+    return single_exchange_rate if on.present?
+
+    multiple_exchange_rates
+  end
+
+  def single_exchange_rate
+    ExchangeRate.find_rate(on, base_currency, counter_currency)
+  rescue ExchangeRate::NotFound => e
+    {error: e.message}
+  end
+
+  def multiple_exchange_rates
+    return {error: "The 'from' date must be older than the 'to' date."} if from > to
+
+    ExchangeRate.rates_between(from, to, base_currency, counter_currency)
   end
 end
